d9f62f3bbd8daef1142f82cc31beb2ac6106f003
SynthKitten <56303691+SynthKittenDev@users.noreply.github.com>
2022-07-12 00:14:55 -0700

    Add files via upload

---
+++ ColonyPlayer/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿
+namespace ColonyPlayer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.axShockwaveFlash1 = new AxShockwaveFlashObjects.AxShockwaveFlash();
+            ((System.ComponentModel.ISupportInitialize)(this.axShockwaveFlash1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // axShockwaveFlash1
+            // 
+            this.axShockwaveFlash1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.axShockwaveFlash1.Enabled = true;
+            this.axShockwaveFlash1.Location = new System.Drawing.Point(0, 0);
+            this.axShockwaveFlash1.Name = "axShockwaveFlash1";
+            this.axShockwaveFlash1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axShockwaveFlash1.OcxState")));
+            this.axShockwaveFlash1.Size = new System.Drawing.Size(800, 575);
+            this.axShockwaveFlash1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(800, 575);
+            this.Controls.Add(this.axShockwaveFlash1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.HelpButton = true;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.axShockwaveFlash1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        private AxShockwaveFlashObjects.AxShockwaveFlash axShockwaveFlash1;
+
+        //private AxShockwaveFlashObjects.AxShockwaveFlash axShockwaveFlash1;
+
+        #endregion
+
+        //private AxShockwaveFlashObjects.AxShockwaveFlash axShockwaveFlash1;
+    }
+}
+
---
+++ ColonyPlayer/Form1.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ColonyPlayer
+{
+    public partial class Form1 : Form
+    {
+
+        // Program opens!
+        public Form1()
+        {
+            // We try to check if Flash10t.ocx is registered or not.. (required for program + game to run)
+            try
+            {
+                using (RegistryKey Key = Registry.ClassesRoot.OpenSubKey(@"TypeLib\{57A0E746-3863-4D20-A811-950C84F1DB9B}\1.1\0\win32"))
+                {
+                    if (Key != null)
+                    {
+                        // Flash10t.ocx is registered. Skipping..
+                    }
+                    else
+                    {
+                        // Flash10t.ocx is not registered to registry. Adding it..
+                        Process proc = new Process();
+                        proc.StartInfo.FileName = "regsvr32.exe";
+                        proc.StartInfo.Arguments = "Flash10t.ocx";
+                        proc.StartInfo.UseShellExecute = false;
+                        proc.StartInfo.CreateNoWindow = true;
+                        proc.StartInfo.RedirectStandardOutput = true;
+                        proc.Start();
+                        string error = proc.StandardOutput.ReadToEnd();
+                        string errorb = proc.StandardError.ReadToEnd();
+                        proc.WaitForExit();
+                        proc.Close();
+                    }
+                }
+                InitializeComponent(); // Run main program
+            }
+            // If there's an error, display it
+            catch (Exception ex)
+            {
+                MessageBox.Show("An error has occured starting Colony, please report this to the Colony discord: " + ex.Message, "Error!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // When program loads, attempt to write the game file to current application directory
+            try
+            {
+                File.WriteAllBytes("ColonyV63.swf", Properties.Resources.ColonyV63); // Write game file to disc from application
+
+                string currentDir = AppDomain.CurrentDomain.BaseDirectory; // Get current application directory
+
+                axShockwaveFlash1.Movie = currentDir + @"/ColonyV63.swf"; // Run game!
+            }
+            // If there is an error, display it!
+            catch (Exception ex)
+            {
+                MessageBox.Show("An error has occured starting Colony, please report this to the Colony discord: " + ex.Message, "Error!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
